d3cf247616116e6b1568f540276d1c876c629a68
RobertoZeppilli <robzep171@gmail.com>
2021-04-29 13:16:41 +0200

    commento

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 // 1. creo un ciclo for per stampare i numeri da 1 a 100
 for (var i = 1; i <= 100; i++) {
 
-    // 2. prendo il primo numero che è sia un multiplo di 3 che di 5, se ha resto = 0 stampo "FizzBuzz"
+    // 2. prendo il primo numero che è sia un multiplo di 3 che di 5, se ha resto = 0 stampo "FizzBuzz", in questo modo anche tutti i successivi multipli di entrambi i numeri avranno stampato "FizzBuzz"
     if (i % 15 == 0) {
         console.log("FizzBuzz");
 
