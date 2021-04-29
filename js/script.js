492a9fc58de85eb680e5a7fc0d67c4b177979b9b
RobertoZeppilli <robzep171@gmail.com>
2021-04-29 22:06:00 +0200

    FizzBuzz(on HTML)

--- js/script.js
+++ js/script.js
@@ -1,24 +1,37 @@
-// Scrivi un programma che stampi i numeri da 1 a 100,
-// ma per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi Buzz.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+// 1. array vuoto in cui inserire successivamente i numeri stabiliti nel primo ciclo for
+var numeri = [];
 
-// 1. creo un ciclo for per stampare i numeri da 1 a 100
+// 2. creo un ciclo for per stampare i numeri da 1 a 100
 for (var i = 1; i <= 100; i++) {
 
-    // 2. prendo il primo numero che è sia un multiplo di 3 che di 5, se ha resto = 0 stampo "FizzBuzz", in questo modo anche tutti i successivi multipli di entrambi i numeri avranno stampato "FizzBuzz"
+    // 2.1 prendo il primo numero che è sia un multiplo di 3 che di 5, se ha resto = 0 stampo "FizzBuzz", in questo modo anche tutti i successivi multipli di entrambi i numeri avranno stampato "FizzBuzz"
     if (i % 15 == 0) {
-        console.log("FizzBuzz");
+        numeri.push("FizzBuzz");
+        
 
-        // 3. se i % 5 da resto = 0 stampo "Buzz" in console    
+
+        // 2.2 se i % 5 da resto = 0 stampo "Buzz" in console    
     } else if (i % 5 == 0) {
-        console.log("Buzz");
+        numeri.push("Buzz");
+        
 
-        // 4. se i % 3 da resto = 0 stampo "Fizz"    
+        // 2.3 se i % 3 da resto = 0 stampo "Fizz"    
     } else if (i % 3 == 0) {
-        console.log("Fizz");
+        numeri.push("Fizz");
+        
 
-        // 5. infine nei casi in cui il numero non è un multiplo ne di 3 ne di 5, stampo il numero    
+        // 2.4 infine nei casi in cui il numero non è un multiplo ne di 3 ne di 5, stampo il numero    
     } else {
-        console.log(i);
+        numeri.push(i);
+        
     }
 }
+
+// 3. secondo ciclo for per stampare nell'HTML una lista con ciò che è nella console
+for (var i = 0; i < 100; i++) {
+    document.getElementById("list").innerHTML += "<li>" + numeri[i] + "</li>";
+}
+
+
+
+
